54d8b969d0f4b19346d067d14bded40eeda87a80
pedro.darde <pedro.darde@retta.com.br>
2022-04-19 23:43:38 -0300

    feat/ .env.example

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,8 +1,8 @@
-import '../database/connection'
+import "../database/connection";
 import app from "./config/app";
 import dotenv from "dotenv";
 dotenv.config();
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3335, () => {
   console.log(`Server running at ${process.env.PORT}`);
 });
